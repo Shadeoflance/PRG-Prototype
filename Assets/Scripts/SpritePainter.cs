83d6f31979d4b525b0c8453de4b3a7c048d3f429
Shadeoflance <shadeoflance@gmail.com>
2016-04-18 22:49:19 +0300

    SpritePainter improve

--- Assets/Scripts/SpritePainter.cs
+++ Assets/Scripts/SpritePainter.cs
@@ -5,7 +5,7 @@ public class SpritePainter : MonoBehaviour
 {
     SpriteRenderer sprite;
     Color initial;
-    List<ColorChanger> changers = new List<ColorChanger>();
+    Group<ColorChanger> changers = new Group<ColorChanger>();
     
     void Start()
     {
@@ -13,18 +13,31 @@ public class SpritePainter : MonoBehaviour
         initial = sprite.color;
     }
 
-    public void Paint(Color c, float time, bool smooth)
+    public void Paint(Color c, float? time, bool smooth)
     {
         changers.Add(new ColorChanger(c, time, smooth));
+    }
+
+    public void Paint(Color c)
+    {
+        Paint(c, null, false);
+    }
+
+    public void Clear()
+    {
+        changers.Clear();
     }
 
     void Update()
     {
         Color c = initial.Copy();
+        changers.Refresh();
         foreach (var a in changers)
         {
             a.Update();
             c = a.Change(c);
+            if (a.time != null && a.time < 0)
+                changers.Remove(a);
         }
         sprite.color = c;
     }
@@ -32,22 +45,28 @@ public class SpritePainter : MonoBehaviour
     private class ColorChanger
     {
         private bool smooth;
-        private float time, initialTime;
+        private float initialTime;
+        public float? time;
         private Color color;
-        public ColorChanger(Color c, float time, bool smooth)
+        public ColorChanger(Color c, float? time, bool smooth)
         {
             this.time = time;
             this.smooth = smooth;
-            initialTime = time;
+            if (time != null)
+                initialTime = time.Value;
             color = c;
         }
         public Color Change(Color c)
         {
-            return Color.Lerp(c, color, smooth ? time / initialTime : 1f);
+            float smoothValue = 1;
+            if (time != null && smooth)
+                smoothValue = time.Value / initialTime;
+            return Color.Lerp(c, color, smoothValue);
         }
         public void Update()
         {
-            time -= Time.deltaTime;
+            if(time != null)
+                time -= Time.deltaTime;
         }
     }
 }
