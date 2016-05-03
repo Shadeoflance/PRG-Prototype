93914d2438e97c328fe9bf0043d21aae9faf80ad
Shadeoflance <shadeoflance@gmail.com>
2016-05-03 22:17:13 +0300

    Implement Tile Editor

---
+++ Assets/Scripts/Editor/GridEditor.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(TileEditor))]
+public class GridEditor : Editor
+{
+    TileEditor tileEditor;
+    Transform parent;
+    public override void OnInspectorGUI()
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Grid Width");
+        tileEditor.width = EditorGUILayout.FloatField(tileEditor.width, GUILayout.Width(50));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Grid Height");
+        tileEditor.height = EditorGUILayout.FloatField(tileEditor.height, GUILayout.Width(50));
+        GUILayout.EndHorizontal();
+        tileEditor.color = EditorGUILayout.ColorField("Grid Color", tileEditor.color);
+        EditorGUILayout.ObjectField("Current Object", tileEditor.current, typeof(GameObject), true);
+    }
+    void Awake()
+    {
+        tileEditor = (TileEditor)target;
+        tileEditor.Init();
+        parent = GameObject.Find("TilesTemp").transform;
+        //SceneView.onSceneGUIDelegate = GridUpdate;
+    }
+
+    public override bool HasPreviewGUI()
+    {
+        return true;
+    }
+
+    public override void OnPreviewGUI(Rect r, GUIStyle background)
+    {
+        if (tileEditor.current == null)
+        {
+            GUI.Label(r, "null");
+            return;
+        }
+        SpriteRenderer sr = tileEditor.current.GetComponent<SpriteRenderer>();
+        if(sr == null)
+            sr = tileEditor.current.transform.FindChild("Sprite").GetComponent<SpriteRenderer>();
+        if (sr == null)
+        {
+            return;
+        }
+        Rect srr = sr.sprite.rect;
+        float width = r.width;
+        r.width = r.height * srr.width / srr.height;
+        r.x = width / 2 - r.width / 2;
+        GUI.DrawTexture(r, sr.sprite.texture);
+    }
+
+    void OnSceneGUI()
+    {
+        Event e = Event.current;
+        OnPreviewGUI(GUILayoutUtility.GetRect(500, 500), EditorStyles.whiteLabel);
+
+        Ray r = Camera.current.ScreenPointToRay(new Vector3(e.mousePosition.x, -e.mousePosition.y + Camera.current.pixelHeight));
+        Vector3 mousePos = r.origin;
+        Vector3 aligned = new Vector3(Mathf.Floor(mousePos.x / tileEditor.width) * tileEditor.width + tileEditor.width / 2.0f,
+                                      Mathf.Floor(mousePos.y / tileEditor.height) * tileEditor.height + tileEditor.height / 2.0f, 0.0f);
+        if (e.isKey)
+        {
+            if (e.character == 'a')
+            {
+                DestroyTileAt(aligned);
+                GameObject obj;
+                Object prefab = tileEditor.current == null ? PrefabUtility.GetPrefabParent(Selection.activeObject) : tileEditor.current;
+                if (prefab)
+                {
+                    obj = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+                    obj.transform.position = aligned;
+                    obj.transform.SetParent(parent);
+                    Undo.RegisterCreatedObjectUndo(obj, "Create " + obj.name);
+                }
+            }
+            else if (e.character == 'd')
+            {
+                DestroyTileAt(aligned);
+            }
+            else if(e.character == 'e')
+            {
+                tileEditor.SelectPrev();
+                e.Use();
+            }
+        }
+        if(e.type == EventType.ScrollWheel)
+        {
+            if (e.delta.y > 0)
+                tileEditor.SelectNext();
+            if (e.delta.y < 0)
+                tileEditor.SelectPrev();
+            e.Use();
+        }
+
+    }
+
+    void DestroyTileAt(Vector3 vec)
+    {
+        foreach (Transform obj in parent)
+        {
+            if (obj.position == vec)
+                Undo.DestroyObjectImmediate(obj.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TileEditor/TileEditor.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TileEditor : MonoBehaviour
+{
+    public float width = 0.5f;
+    public float height = 0.5f;
+    public GameObject current;
+    int currentInd = 0;
+    public List<GameObject> tiles;
+
+    public Color color = Color.white;
+
+    public void Init()
+    {
+        tiles = new List<GameObject>();
+        tiles.AddRange(Resources.LoadAll<GameObject>("Level/Tiles"));
+        current = tiles[0];
+    }
+
+    public void SelectNext()
+    {
+        currentInd = (currentInd + 1) % tiles.Count;
+        current = tiles[currentInd];
+    }
+
+    public void SelectPrev()
+    {
+        currentInd = (currentInd - 1 + tiles.Count) % tiles.Count;
+        current = tiles[currentInd];
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector3 pos = Camera.current.transform.position;
+
+        Gizmos.color = color;
+
+        width = width < 0.01f ? 0.01f : width;
+        height = height < 0.01f ? 0.01f : height;
+        for (float y = pos.y - 800.0f; y < pos.y + 800.0f; y += height)
+        {
+            Gizmos.DrawLine(new Vector3(-1000000.0f, Mathf.Floor(y / height) * height, 0.0f),
+                            new Vector3(1000000.0f, Mathf.Floor(y / height) * height, 0.0f));
+        }
+
+        for (float x = pos.x - 1200.0f; x < pos.x + 1200.0f; x += width)
+        {
+            Gizmos.DrawLine(new Vector3(Mathf.Floor(x / width) * width, -1000000.0f, 0.0f),
+                            new Vector3(Mathf.Floor(x / width) * width, 1000000.0f, 0.0f));
+        }
+    }
+}
